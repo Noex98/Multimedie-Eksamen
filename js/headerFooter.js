48eb46e63aef183da6359503b1b1abb4c7307ecc
Noex98 <johannes.knickering@hotmail.com>
2021-05-23 21:02:37 +0200

    .

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -20,11 +20,7 @@ function burgerClick(){
 
 	//animate Links
 	navLinks.forEach((link, index) => {
-		if (link.style.animation) {
-			link.style.animation = ''
-		} else {
-			link.style.animation =`navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
-		}
+		link.style.animation = link.style.animation? '' : link.style.animation =`navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
 	});
 
 	//Burger Animation
@@ -46,8 +42,6 @@ addEventListener('scroll', () => {
         header.classList.add('header--scrolled')
     }
 });
-
-
 
 
 //#########
